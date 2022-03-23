b656747f7f79186b1388479bed54b71ccf491d35
binkaGR <102261632+binkaGR@users.noreply.github.com>
2022-03-24 00:29:21 +0200

    Add files via upload

---
+++ StudentInfoSystem/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    class Student
+    {
+        public string Name { get;set;}
+        public string Second_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Fac_Number { get; set; }
+        public string Specialty { get; set; }
+        public string State { get; set; }
+        public string Course { get; set; }
+        public string Potok { get; set; }
+        public string Group { get; set; }
+        public Student()
+        {
+            this.Name = "Binka";
+            this.Second_Name ="Grozdanova";
+            this.Last_Name = "Aleksandrova";
+            this.Fac_Number = "121219038";
+            this.Specialty = "KSI";
+            this.State = "редовно";
+            this.Course = "3";
+            this.Potok = "9";
+            this.Group = "29";
+        }
+    }
+}
---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    class StudentData
+    {
+        public static List<Student> TestStudents;
+
+        public static void Add_TestStudents()
+        {
+            TestStudents = new List<Student>();
+            TestStudents.Add(new Student()
+            {
+                Name= "Binka",
+                Second_Name="Grozdanova",
+                Last_Name="Aleksandrova",
+                Fac_Number= "121219038",
+                Specialty="FKST",
+                State="редовно",
+                Course="3",
+                Potok="9",
+                Group="30"
+            });
+        }
+
+
+    }
+}
